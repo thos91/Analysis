819a66c1daaa5096c9c68e12107d742a2f5fe2fd
Eguchi Aoi <eguchi@hep.phys.s.u-tokyo.ac.jp>
2019-06-18 12:53:19 +0900

    Modified the wgScurve program

---
+++ include/wgScurve.hpp
@@ -0,0 +1,49 @@
+#ifndef WG_SCURVE_HPP_
+#define WG_SCURVE_HPP_
+
+// system C++ includes
+#include <string>
+
+// system C includes
+#include <cstdbool>
+
+// user includes
+#include "Const.hpp"
+
+// #define M 5
+
+// #define SELECT_NOISE     0
+// #define SELECT_GAIN      1
+// #define SELECT_PEDESTAL  2
+// #define SELECT_RAWCHARGE 3
+// #define SELECT_PRINT     4
+
+#define SCURVE_SUCCESS              0
+#define ERR_CANNOT_CREATE_DIRECTORY 1
+#define ERR_WRONG_MODE              2
+#define ERR_FAILED_CREATE_XML_FILE  3
+#define ERR_WG_SCURVE               4
+
+using namespace std;
+
+// This is needed to call the following functions from Python using ctypes
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  double Calcurate_Mean(vector<double>);
+  double Calcurate_Sigma(vector<double>);
+  
+  double NoiseToPe(double); // where is this function used??
+  void MakeDir(string& str);
+  void MakeXML(string& str,int ichip);
+  vector<string> GetIncludeFileName(string& inputDirName);
+  void AnaXML(vector<string> &inputDirName,string& outputXMLDirName,string& outputIMGDirName,int ichip);
+  int wgScurve(const char* x_inputDirName, const char* x_outputXMLDirName, const char* x_outputIMGDirName);
+
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WG_SCURVE_HPP_
---
+++ process/wgScurve/wgScurve.rst
@@ -0,0 +1,15 @@
+========================
+wgScurve program
+========================
+
+The wgScurve program is used to draw noise rate S-curve 
+from the xml files created by the wgAnaHistSummary program.
+
+Arguments
+=========
+
+- [-h] : prints an help message
+- [-f] : input directory with xml files to read (mandatory)
+- [-o] : output directory for the xml summary files (default: same as input directory)
+
+
