e5e6559499fb89ea23aa4df2745afc55027d9e05
Giorgio Pintaudi <giorgio.pimpa@gmail.com>
2019-07-19 12:01:56 +0900

    Fixed wgDecoder
    No more using namespace std
    Better way to read start_time, stop_time, etc... from log file

--- process/wgDecoder/libwgDecoder.cpp
+++ process/wgDecoder/libwgDecoder.cpp
@@ -3,23 +3,19 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <bitset>
 #include <vector>
-#include <limits>
-
-// system C includes
-#include "unistd.h"
-#include <bits/stdc++.h>
+#include <iomanip>
+#include <locale>
 
 // boost includes
 #include <boost/filesystem.hpp>
 
 // ROOT includes
-#include "TDirectory.h"
 #include "TFile.h"
-#include "TH1.h"
-#include "TH2.h"
+#include "TH1I.h"
 #include "TTree.h"
-#include "TInterpreter.h"
+#include "TParameter.h"
 
 // user includes
 #include "wgConst.hpp"
@@ -34,7 +30,6 @@
 #include "wgDecoderUtils.hpp"
 #include "wgLogger.hpp"
 
-using namespace std;
 using namespace wagasci_tools;
 
 int wgDecoder(const char * x_input_raw_file,
@@ -45,10 +40,10 @@ int wgDecoder(const char * x_input_raw_file,
               unsigned dif,
               unsigned n_chips) {
 
-  string input_raw_file(x_input_raw_file);
-  string calibration_dir(x_calibration_dir);
-  string output_dir(x_output_dir);
-  string output_file_name = GetName(input_raw_file) + "_tree.root";
+  std::string input_raw_file(x_input_raw_file);
+  std::string calibration_dir(x_calibration_dir);
+  std::string output_dir(x_output_dir);
+  std::string output_file_name = GetName(input_raw_file) + "_tree.root";
 
   // ===================================================================== //
   //                         Arguments sanity check                        //
@@ -71,7 +66,7 @@ int wgDecoder(const char * x_input_raw_file,
   // ======== dif ========= //
 
   if (dif > NDIFS) {
-    Log.eWrite("[wgDecoder] The DIF number must be {1-" + to_string(NDIFS) + "}");
+    Log.eWrite("[wgDecoder] The DIF number must be {1-" + std::to_string(NDIFS) + "}");
     exit(1);
   }
   
@@ -82,7 +77,7 @@ int wgDecoder(const char * x_input_raw_file,
   }
   if (n_chips == 0 || n_chips > NCHIPS) {
     Log.eWrite("[wgDecoder] The number of chips per DIF must be {1-"
-               + to_string(NCHIPS) + "}");
+               + std::to_string(NCHIPS) + "}");
     exit(1);
   }
 
@@ -90,7 +85,7 @@ int wgDecoder(const char * x_input_raw_file,
 
   // This is not the output log file but the log file that should be already
   // present in the input folder and was created together with the .raw file
-  string pyrame_log_file  = GetName(input_raw_file);
+  std::string pyrame_log_file  = GetName(input_raw_file);
   size_t pos  = pyrame_log_file.rfind("_ecal_dif_") ;
   pyrame_log_file = GetPath(input_raw_file) + pyrame_log_file.substr(0, pos) + ".log";
 
@@ -98,7 +93,7 @@ int wgDecoder(const char * x_input_raw_file,
   
   try { MakeDir(output_dir); }
   catch (const wgInvalidFile& e) {
-    Log.eWrite("[wgDecoder] " + string(e.what()));
+    Log.eWrite("[wgDecoder] " + std::string(e.what()));
     return ERR_FAILED_CREATE_DIRECTORY;
   }
 
@@ -114,39 +109,22 @@ int wgDecoder(const char * x_input_raw_file,
   // the file name
   if (dif == 0) {
     std::string input_raw_file_name = wagasci_tools::GetName(input_raw_file);
-    if ((pos = input_raw_file_name.find("dif_1_1_")) != string::npos) {
+    if ((pos = input_raw_file_name.find("dif_1_1_")) != std::string::npos) {
       try {
         dif = stoi(input_raw_file_name.substr(pos + 8, pos + 9));
       } catch (const std::invalid_argument& e) {
-        Log.eWrite("[wgDecoder] failed to read the DIF number from the file name : " + string(e.what()));
+        Log.eWrite("[wgDecoder] failed to read the DIF number from the file name : " + std::string(e.what()));
       }
-    } else if ((pos = input_raw_file_name.find("dif_")) != string::npos) {
+    } else if ((pos = input_raw_file_name.find("dif_")) != std::string::npos) {
       try {
         dif = stoi(input_raw_file_name.substr(pos + 4, pos + 5)) + 1;
       } catch (const std::invalid_argument& e) {
-        Log.eWrite("[wgDecoder] failed to read the DIF number from the file name : " + string(e.what()));
+        Log.eWrite("[wgDecoder] failed to read the DIF number from the file name : " + std::string(e.what()));
       }
     } else {
       Log.eWrite("[wgDecoder] Error: DIF ID number not given nor found");
       return ERR_WRONG_DIF_VALUE;
     }
-  }
-
-  // ===================================================================== //
-  //                         Create the output file                        //
-  // ===================================================================== //
-
-  TFile * outputTFile;
-  TString output_file_path(output_dir + "/" + output_file_name);
-  if (!overwrite) {
-    if (check_exist::RootFile(output_file_path)) {
-      Log.eWrite("[wgDecoder] Error:" + string(output_file_path.Data()) +
-                 " already exists!");
-      return ERR_OVERWRITE_FLAG_NOT_SET;
-    }
-    outputTFile = new TFile(output_file_path, "create");
-  } else {
-    outputTFile = new TFile(output_file_path, "recreate");
   }
 
   // ===================================================================== //
@@ -171,24 +149,24 @@ int wgDecoder(const char * x_input_raw_file,
         calib.GetPedestal(dif, rd.pedestal);
     } catch (const exception & e ) {
       pedestal_is_calibrated = false;
-      Log.Write("[wgDecoder] pedestal is not calibrated yet : " + string(e.what()));
+      Log.Write("[wgDecoder] pedestal is not calibrated yet : " + std::string(e.what()));
     }
     try {
       if ((gain_is_calibrated = calib.isGainCalibrated()))
         calib.GetGain(dif, rd.gain);
     } catch (const exception & e ) {
       adc_is_calibrated = false;
-      Log.Write("[wgDecoder] ADC is not calibrated yet : " + string(e.what()));
+      Log.Write("[wgDecoder] ADC is not calibrated yet : " + std::string(e.what()));
     }
     try {
       if ((tdc_is_calibrated = calib.isTDCCalibrated()))
         calib.GetTDC(dif, rd.tdc_slope, rd.tdc_intcpt);
     } catch (const exception & e ) {
       tdc_is_calibrated = false;
-      Log.Write("[wgDecoder] TDC is not calibrated yet : " + string(e.what()));
+      Log.Write("[wgDecoder] TDC is not calibrated yet : " + std::string(e.what()));
     }  
   } catch (const exception & e ) {
-    Log.Write("[wgDecoder] detector is not calibrated yet : " + string(e.what())); 
+    Log.Write("[wgDecoder] detector is not calibrated yet : " + std::string(e.what())); 
   }
   adc_is_calibrated = pedestal_is_calibrated && gain_is_calibrated;
 
@@ -214,61 +192,33 @@ int wgDecoder(const char * x_input_raw_file,
                   rd.z   [ichip].data());
     }
   }
-  
+
   // ===================================================================== //
-  //                        Read Pyrame log file                           //
+  //                         Create the output file                        //
   // ===================================================================== //
 
-  if( check_exist::LogFile(pyrame_log_file) ) {
-    
-    // Will be filled with  v[0]: start_time, v[1]: stop_time, v[2]: nb_data_pkts, v[3]: nb_lost_pkts
-    vector<int> v_log(4,0);
-    wgEditXML edit;
-    edit.GetLog(pyrame_log_file, v_log);
-
-    time_t current_time = time(0);
-    tm *tm = localtime(&current_time);
-    string this_year(Form("%04d-%02d-%02d-%02d-%02d", tm->tm_year + 1900, 1, 1, 0, 0));
-    string next_year(Form("%04d-%02d-%02d-%02d-%02d", tm->tm_year + 1900 + 1, 1, 1, 0, 0));
-    struct tm date;
-    strptime(this_year.c_str(), "%Y-%m-%d-%H-%M", &date);
-    int datetime_str = (int) mktime(&date);
-    strptime(next_year.c_str(), "%Y-%m-%d-%H-%M", &date);
-    int datetime_end = (int) mktime(&date);
-    int datetime_bin = datetime_end - datetime_str;
-
-    // Start time of the acquisition that produced the .raw file
-    TH1D * h_start_time = new TH1D("start_time", "Time the acquisition started", datetime_bin, datetime_str, datetime_end);
-    h_start_time->Fill(v_log[0], v_log[0]);
-    h_start_time->Fill(v_log[0] + 1, v_log[0] - datetime_end);
-    // Stop time of the acquisition that produced the .raw file
-    TH1D * h_stop_time = new TH1D("stop_time", "Time the acquisition stopped", datetime_bin, datetime_str, datetime_end);
-    h_stop_time->Fill(v_log[1], v_log[1]);
-    h_stop_time->Fill(v_log[1] + 1, v_log[1] - datetime_end);
-    // Number of data packets acquired as reported by Pyrame in the acquisition log file
-    TH1D * h_nb_data_pkts = new TH1D("nb_data_pkts", "Number of data packets", datetime_bin, datetime_str, datetime_end);
-    h_nb_data_pkts->Fill(v_log[2]);
-    // Number of lost packets acquired as reported by Pyrame in the acquisition log file
-    TH1D * h_nb_lost_pkts = new TH1D("nb_lost_pkts", "Number of lost packests", datetime_bin, datetime_str, datetime_end);
-    h_nb_lost_pkts->Fill(v_log[3]);
-
-    outputTFile->cd();
-    h_start_time  ->Write();
-    h_stop_time   ->Write();
-    h_nb_data_pkts->Write();
-    h_nb_lost_pkts->Write();
-    delete h_start_time;
-    delete h_stop_time;
-    delete h_nb_data_pkts;
-    delete h_nb_lost_pkts;
+  TFile * output_file;
+  TString output_file_path(output_dir + "/" + output_file_name);
+  if (!overwrite) {
+    if (check_exist::RootFile(output_file_path)) {
+      Log.eWrite("[wgDecoder] Error:" + std::string(output_file_path.Data()) +
+                 " already exists!");
+      return ERR_OVERWRITE_FLAG_NOT_SET;
+    }
+    output_file = new TFile(output_file_path, "create");
+  } else {
+    output_file = new TFile(output_file_path, "recreate");
   }
-  else Log.eWrite("[wgDecoder ] Pyrame log file : " + pyrame_log_file + " doesn't exist!");
+
 
   // ===================================================================== //
   //                      Create TTree branches                            //
   // ===================================================================== //
 
-  TTree * tree = new TTree("tree", "ROOT tree containing decoded data");
+  TString tree_name("tree_dif_" + std::to_string(dif));
+  TString tree_title("ROOT tree containing decoded data : DIF " + std::to_string(dif));
+  TTree * tree = new TTree(tree_name, tree_title);
+  tree->SetDirectory(output_file);
   tree->Branch("spill_number",&rd.spill_number     ,"spill_number/I"                                               );
   tree->Branch("spill_mode"  ,&rd.spill_mode       ,"spill_mode/I"                                                 );
   tree->Branch("spill_count" ,&rd.spill_count      ,"spill_count/I"                                                );
@@ -301,6 +251,25 @@ int wgDecoder(const char * x_input_raw_file,
     tree->Branch("tdc_intcpt",rd.tdc_intcpt.data() ,Form("tdc_intcpt[%d][%d][%d]/D" ,n_chips, NCHANNELS, 2        ));
   }
 
+  // ===================================================================== //
+  //                        Read Pyrame log file                           //
+  // ===================================================================== //
+
+  if( check_exist::LogFile(pyrame_log_file) ) {
+    // Will be filled with  v[0]: start_time, v[1]: stop_time, v[2]: nb_data_pkts, v[3]: nb_lost_pkts
+    std::vector<std::string> v_log;
+    wgEditXML edit;
+    edit.GetLog(pyrame_log_file, v_log);
+    // Start time of the acquisition that produced the .raw file
+    tree->GetUserInfo()->Add(new TParameter<Int_t>("start_time", datetime::DatetimeToSeconds(v_log[0])));
+    // Stop time of the acquisition that produced the .raw file
+    tree->GetUserInfo()->Add(new TParameter<Int_t>("stop_time", datetime::DatetimeToSeconds(v_log[1])));
+    // Number of data packets acquired as reported by Pyrame in the acquisition log file
+    tree->GetUserInfo()->Add(new TParameter<Int_t>("nb_data_pkts", std::stoi(v_log[2])));
+    // Number of lost packets acquired as reported by Pyrame in the acquisition log file
+    tree->GetUserInfo()->Add(new TParameter<Int_t>("nb_lost_pkts", std::stoi(v_log[3])));
+  }
+  else Log.eWrite("[wgDecoder]  Pyrame log file : " + pyrame_log_file + " doesn't exist!");
 
   // ===================================================================== //
   //                Allocate the RawDataConfig class object                //
@@ -326,7 +295,7 @@ int wgDecoder(const char * x_input_raw_file,
   ifstream ifs;
   ifs.open(input_raw_file.c_str(), ios_base::in | ios_base::binary);
   if (!ifs.is_open()) {
-    Log.eWrite("[wgDecoder] Failed to open raw file: " + string(strerror(errno)));
+    Log.eWrite("[wgDecoder] Failed to open raw file: " + std::string(strerror(errno)));
     return ERR_FAILED_OPEN_RAW_FILE;
   }
 
@@ -366,7 +335,7 @@ int wgDecoder(const char * x_input_raw_file,
       if (current_section.type == SectionSeeker::SectionType::SpillTrailer) {
         rd.clear();
         if ((n_good_spills + n_bad_spills) % 1000 == 0) {
-          Log.Write("[wgDecoder] Decoded " + to_string(n_good_spills + n_bad_spills) + " spills");
+          Log.Write("[wgDecoder] Decoded " + std::to_string(n_good_spills + n_bad_spills) + " spills");
         }
       }
       
@@ -374,24 +343,23 @@ int wgDecoder(const char * x_input_raw_file,
     }
   } catch (const wgEOF& e) {
     result = WG_SUCCESS;
-  } catch (const exception& e) {
-    Log.eWrite("[wgDecoder] Error while reading raw data : " + string(e.what()));
+  } catch (const std::exception& e) {
+    Log.eWrite("[wgDecoder] Error while reading raw data : " + std::string(e.what()));
     result = ERR_WG_DECODER;
   }
 
   // ===================================================================== //
   //                           Close everything                            //
   // ===================================================================== //
-  
-  Log.Write("[wgDecoder] *****  GOOD spills : " + to_string(n_good_spills) + " spills *****");
-  Log.Write("[wgDecoder] *****  BAD  spills : " + to_string(n_bad_spills) + " spills *****");
 
   ifs.close();
-  outputTFile->cd();
   tree->Write();
-  outputTFile->Write();
-  outputTFile->Close();
-  delete outputTFile;
+  output_file->Close();
+  delete output_file;
 
+  Log.Write("[wgDecoder] *****  GOOD spills : " + std::to_string(n_good_spills) + " spills *****");
+  Log.Write("[wgDecoder] *****  BAD  spills : " + std::to_string(n_bad_spills) + " spills *****");
+  
   return result;
 }
+
