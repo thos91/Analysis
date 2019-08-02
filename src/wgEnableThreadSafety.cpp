a000da6271089ec77ac5c734147b8a53976fb05a
Giorgio Pintaudi <giorgio.pimpa@gmail.com>
2019-08-03 04:05:02 +0900

    Use the Minuit2 minimizer because it is the only one that is thread-safe

--- src/wgEnableThreadSafety.cpp
+++ src/wgEnableThreadSafety.cpp
@@ -1,11 +1,19 @@
 // ROOT includes
 #include "TROOT.h"
+#include "TPluginManager.h"
+#include "TMinuitMinimizer.h"
 
 // user includes
+#include "wgLogger.hpp"
 #include "wgEnableThreadSafety.hpp"
 
 void wgEnableThreadSafety() {
   ROOT::EnableThreadSafety();
+  // We will be using Minuit2 for fitting since it is thread safe, so check it is there
+  if (gROOT->GetPluginManager()->FindHandler("ROOT::Math::Minimizer", "Minuit2") == 0)  {
+    Log.eWrite("We need Minuit2 for parallel fitting!");
+  } else 
+    ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2");
 }
 
 RootSideEffectGuard::RootSideEffectGuard(): m_directory(gDirectory) {
