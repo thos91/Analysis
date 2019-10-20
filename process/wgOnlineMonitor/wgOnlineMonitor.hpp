ae76e8a985a8ab30e335dc10fe30e8b3922aecf8
Giorgio Pintaudi <giorgio.pimpa@gmail.com>
2019-10-20 20:08:58 +0900

    Removed useless stuff from wgOnlineMonitor.hpp

--- process/wgOnlineMonitor/wgOnlineMonitor.hpp
+++ process/wgOnlineMonitor/wgOnlineMonitor.hpp
@@ -1,17 +1,6 @@
 #ifndef WGONLINEMONITOR_H
 #define WGONLINEMONITOR_H
 
-// ROOT includes
-// #include <TROOT.h>
-// #include <TH1F.h>
-// #include <TH2F.h>
-// #include <TCanvas.h>
-// #include <TGraph.h>
-// #include <TText.h>
-
-#define NHIST   10    // Total number of the Histograms that are shown
-#define NPLANES 8     // Number of planes
-
 struct om_ws 
 {
   int dif_id;
@@ -23,7 +12,6 @@ struct om_ws
   int lastblock_evtdisp; // Last block that was shown in the
                          // real-time event display
   int last_spill;        // spill number of the previous block
-
   int num_spill_gaps;    // number of times a spill number gap has bee detected
 };
 
